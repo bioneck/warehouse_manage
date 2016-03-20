c8a00b7fbb9fb1a59d9ffa7443fbdc32031b7260
huanghaoyu <q891541372@gmail.com>
2016-03-20 22:03:57 +0800

    添加模型

--- management/models.py
+++ management/models.py
@@ -1,5 +1,31 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class warehouse(models.Model):
+	warehousename = models.CharField(max_length = 20)
+	warehouseinfo = models.CharField(max_length = 50)
+	stock = models.CharField(max_length = 20)
+	inventory = models.CharField(max_length = 20)
+
+	def __unicode__(self):
+        return self.warehousename
+
+class goods(models.Model):
+	goodsname = models.CharField(max_length = 20)
+	goodsstock = models.CharField(max_length = 20)
+	goodsinfo = models.CharField(max_length = 50)
+
+	def __unicode__(self):
+        return self.goodsname
+
+class turnover(models.Model):
+	quantity = models.CharField(max_length = 20)
+	date_time = models.DateTimeField(auto_now_add = True)
+
+	def __unicode__(self):
+        return self.goodsname
+        
+    class Meta:  #按时间下降排序
+        ordering = ['-date_time']
