0195fa7ea1c05cc083ed2e5dd3c4c6bf5edd4c82
huanghaoyu <q891541372@gmail.com>
2016-03-21 21:21:42 +0800

    模型添加中文

--- management/admin.py
+++ management/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import warehouse, goods, turnover
 
 # Register your models here.
+admin.site.register(warehouse)
+admin.site.register(goods)
+admin.site.register(turnover)
--- management/models.py
+++ management/models.py
@@ -1,31 +1,32 @@
+# coding:utf-8
 from __future__ import unicode_literals
 
 from django.db import models
 from django.contrib.auth.models import User
 
 class warehouse(models.Model):
-	warehousename = models.CharField(max_length = 20)
-	warehouseinfo = models.CharField(max_length = 50)
-	stock = models.CharField(max_length = 20)
-	inventory = models.CharField(max_length = 20)
+	warehousename = models.CharField(u'仓库名',max_length = 20)
+	warehouseinfo = models.CharField(u'仓库信息',max_length = 50)
+	stock = models.CharField(u'库存',max_length = 20)
+	inventory = models.CharField(u'商品种类',max_length = 20)
 
 	def __unicode__(self):
-        return self.warehousename
+		return self.warehousename
 
 class goods(models.Model):
-	goodsname = models.CharField(max_length = 20)
-	goodsstock = models.CharField(max_length = 20)
-	goodsinfo = models.CharField(max_length = 50)
+	goodsname = models.CharField(u'物品名',max_length = 20)
+	goodsstock = models.CharField(u'库存',max_length = 20)
+	goodsinfo = models.CharField(u'物品信息',max_length = 50)
 
 	def __unicode__(self):
-        return self.goodsname
+		return self.goodsname
 
 class turnover(models.Model):
-	quantity = models.CharField(max_length = 20)
+	quantity = models.CharField(u'数量',max_length = 20)
 	date_time = models.DateTimeField(auto_now_add = True)
 
 	def __unicode__(self):
-        return self.goodsname
+		return self.goodsname
         
-    class Meta:  #按时间下降排序
-        ordering = ['-date_time']
+	class Meta:  #按时间下降排序
+		ordering = ['-date_time']
